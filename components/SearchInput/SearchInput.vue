c999fd54c6cd5a665e3f404aa242bff189f39361
xiaojiang2000 <1773603105@qq.com>
2019-12-17 14:52:12 +0800

    组件

---
+++ components/SearchInput/SearchInput.vue
@@ -0,0 +1,46 @@
+<template>
+<view class="search_input">
+  <navigator class="a" url="../../goodrecommend/haowu/haowu" open-type="navigate">
+    搜索
+  </navigator>
+</view>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {};
+  },
+
+  components: {},
+  props: {},
+  methods: {
+    setData: function (obj, callback) {
+      let that = this;
+      let keys = [];
+      let val, data;
+      Object.keys(obj).forEach(function (key) {
+        keys = key.split('.');
+        val = obj[key];
+        data = that.$data;
+        keys.forEach(function (key2, index) {
+          if (index + 1 == keys.length) {
+            that.$set(data, key2, val);
+          } else {
+            if (!data[key2]) {
+              that.$set(data, key2, {});
+            }
+          }
+
+          data = data[key2];
+        });
+      });
+      callback && callback();
+    }
+  }
+};
+</script>
+<style>
+@import "./SearchInput.css";
+</style>
